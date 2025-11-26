3f16aeef9d75a4a3686fb71b47325cb467a3684f
Gabriel Marques Belo <gabriel_itu2013@hotmail.com>
2025-11-26 13:23:44 -0300

    Criando Exception hankLing - findById

--- src/main/java/com/projeto/spring/aula/curso/services/UserService.java
+++ src/main/java/com/projeto/spring/aula/curso/services/UserService.java
@@ -2,6 +2,7 @@ package com.projeto.spring.aula.curso.services;
 
 import com.projeto.spring.aula.curso.entities.User;
 import com.projeto.spring.aula.curso.repositories.UserRepository;
+import com.projeto.spring.aula.curso.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id){
        Optional<User> obj = repository.findById(id);
-       return obj.get();
+       return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
