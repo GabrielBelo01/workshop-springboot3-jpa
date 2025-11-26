6f1a50eb8267f36cd9de3afbe8e35d45dbe784d5
Gabriel Marques Belo <gabriel_itu2013@hotmail.com>
2025-11-26 11:27:29 -0300

    Criando metodos subTotal e Total

--- src/main/java/com/projeto/spring/aula/curso/entities/Order.java
+++ src/main/java/com/projeto/spring/aula/curso/entities/Order.java
@@ -89,6 +89,14 @@ public class Order implements Serializable {
         return items;
     }
 
+    public Double getTotal(){
+        double soma = 0;
+        for(OrderItem x : items){
+            soma += x.getSubTotal();
+        }
+        return soma;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
